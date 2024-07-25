3bc2efdaa9d6e35347edd2dba8588f9eadd26402
LmX <aksenczuk.alexander@gmail.com>
2024-07-25 17:24:16 +0200

    second commit fix

--- src/bin/components/buttons.js
+++ src/bin/components/buttons.js
@@ -1,24 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import Col from 'react-bootstrap/Col';
-import { Link } from 'react-router-dom';
 
 const Buttons = () => {
-    const handleClickInfo = () => {
-        console.log("info");
-    }
-
-    const handleClickMapa = () => {
-        console.log("mapa");
-    }
-
-    const handleClickSklep = () => {
-        console.log("sklep");
-    }
-    
     return (  
     <Col sm="10" id="Navbar">
-        <a href="https://bluemap.bluecolored.de" target='_blank'><button onClick={handleClickMapa}>MAPA</button></a>
-        <a href="https://vishop.pl/shop/14027/" target='_blank'><button onClick={handleClickSklep}>SKLEP</button></a>
+        <a href="https://bluemap.bluecolored.de" target='_blank' rel="noreferrer"><button onClick={handleClickMapa}>MAPA</button></a>
+        <a href="https://vishop.pl/shop/14027/" target='_blank' rel="noreferrer"><button onClick={handleClickSklep}>SKLEP</button></a>
     </Col>
 );
 }
