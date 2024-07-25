75fa9dfff48dbd29cd85a58d30d7e717d617d6cc
LmX <aksenczuk.alexander@gmail.com>
2024-07-25 17:25:52 +0200

    second commit fix

--- src/bin/components/buttons.js
+++ src/bin/components/buttons.js
@@ -4,8 +4,8 @@ import Col from 'react-bootstrap/Col';
 const Buttons = () => {
     return (  
     <Col sm="10" id="Navbar">
-        <a href="https://bluemap.bluecolored.de" target='_blank' rel="noreferrer"><button onClick={handleClickMapa}>MAPA</button></a>
-        <a href="https://vishop.pl/shop/14027/" target='_blank' rel="noreferrer"><button onClick={handleClickSklep}>SKLEP</button></a>
+        <a href="https://bluemap.bluecolored.de" target='_blank' rel="noreferrer"><button>MAPA</button></a>
+        <a href="https://vishop.pl/shop/14027/" target='_blank' rel="noreferrer"><button>SKLEP</button></a>
     </Col>
 );
 }
