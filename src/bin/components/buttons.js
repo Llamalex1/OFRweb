53b3942b9f6d463e30c2b1dc5fead77f74ecc8ed
LmX <aksenczuk.alexander@gmail.com>
2024-08-19 09:35:22 +0200

    Fixed bluemap connection

--- src/bin/components/buttons.js
+++ src/bin/components/buttons.js
@@ -4,7 +4,7 @@ import Col from 'react-bootstrap/Col';
 const Buttons = () => {
     return (  
     <Col sm="10" id="Navbar">
-        <a href="http://ofrweb.minecraft.best:25566/#world:0:69:0:39:-2.16:0:0:0:perspective" target='_blank' rel="noreferrer"><button>MAPA</button></a>
+        <a href="http://ofrweb.pl:57145/#world:0:69:0:39:-2.16:0:0:0:perspective" target='_blank' rel="noreferrer"><button>MAPA</button></a>
         <a href="https://vishop.pl/shop/14027/" target='_blank' rel="noreferrer"><button>SKLEP</button></a>
     </Col>
 );
